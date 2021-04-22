9a0213c69c9f5d3b4287356684407f366ad295d6
Prajval <er.prajvalbadiger@gmail.com>
2021-04-23 01:32:11 +0530

    added bluetooth status

--- config.def.h
+++ config.def.h
@@ -1,8 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "dwm_bt",						360,	4},
 	{"", "dwm_nw",						2,		10},
-	{"", "dwm_wttr",					360,	10},
+	//{"", "dwm_wttr",					360,	10},
 	{"", "dwm_brightness",				360,	30},
 	{"", "dwm_vol",					    360,	20},
 	{"", "dwm_batt",					5,		3},
